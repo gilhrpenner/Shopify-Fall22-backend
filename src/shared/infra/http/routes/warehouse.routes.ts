0bd121a72d29b6ddaeeb3a0309947981eba5162d
Gil Penner <gilhrpenner@gmail.com>
2022-05-20 10:09:14 -0500

    Added endpoints to fetch all warehouses and products

--- src/shared/infra/http/routes/warehouse.routes.ts
+++ src/shared/infra/http/routes/warehouse.routes.ts
@@ -1,9 +1,14 @@
 import { createWarehouseController } from '@modules/warehouse/createWarehouse';
 import { deleteWarehouseController } from '@modules/warehouse/deleteWarehouse';
+import { getWarehousesController } from '@modules/warehouse/getWarehouses';
 import { updateWarehouseController } from '@modules/warehouse/updateWarehouse';
 import { Router } from 'express';
 
 const warehouseRoutes = Router();
+
+warehouseRoutes.get('/', async (req, res) => {
+    return getWarehousesController.handle(req, res);
+});
 
 warehouseRoutes.post('/', async (req, res) => {
     return createWarehouseController.handle(req, res);
